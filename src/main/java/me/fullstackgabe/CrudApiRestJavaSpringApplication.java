53b35b2ac4496af946b4caa6dddb37bc862b9acf
Gabriel447 <fullstackgabe@gmail.com>
2023-10-15 12:23:29 -0300

    first commit

---
+++ src/main/java/me/fullstackgabe/CrudApiRestJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package me.fullstackgabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiRestJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiRestJavaSpringApplication.class, args);
+	}
+
+}
